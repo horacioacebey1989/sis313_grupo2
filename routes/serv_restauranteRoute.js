dbdc6d55764d618887598492d6606b0dcb8cb94e
Klvmb <klvgas@gmail.com>
2021-10-07 23:26:20 -0400

    Nuevo intento esta vez si esta es la buena la decima es la vencida

---
+++ routes/serv_restauranteRoute.js
@@ -0,0 +1,18 @@
+'use strict'
+
+var express = require('express');
+
+var serv_restauranteController = require('../controllers/serv_restauranteController');
+
+var api = express.Router();
+
+
+api.post('/addserv_restaurante', serv_restauranteController.addserv_restaurante);
+
+api.get('/getserv_restauranteByID', serv_restauranteController.getserv_restaurante);
+
+api.get('/getserv_restauranteByID/:id', serv_restauranteController.getserv_restaurante2);
+
+api.put('/updateserv_restaurante/:id', serv_restauranteController.updateserv_restaurante);
+
+module.exports = api;
