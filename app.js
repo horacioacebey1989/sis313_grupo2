dc353282a31c75cc440f7b20e1ec4f3bf6456c76
horacioacebey1989 <70784236+horacioacebey1989@users.noreply.github.com>
2021-10-11 23:03:15 -0400

    Revert "SIS-10-CRUD-proveedor"

--- app.js
+++ app.js
@@ -1,39 +1,52 @@
 'use strict'
 
 var express = require('express');
-var bodyParser = require ('body-parser');
-//var bcrypt = require('bcrypt-nodejs');
+
+var bodyParser = require('body-parser');
+
+
 
 var app = express();
 
-//Para cargar rutas
-var tipoUsuario_route = require('./routes/tipoUsuario');
-var usuario_route = require('./routes/usuario');
-var serv_agencia_route = require('./routes/serv_agencia');
-var indv_proveedor_route = require('./routes/indv_proveedor');
 
-//Para middlewares
-app.use(bodyParser.urlencoded({extended:true}));
+
+
+var indv_usuario_route = require('./routes/indv_usuarioRoute');
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-//Para rutas
-app.post('/prueba', (req,res) =>{
+
+var tipoUsuario_route = require('./routes/tipoUsuario');
+
+var usuario_route = require('./routes/usuario');
+
+
+
+// RUTAS 
+
+
+app.post('/prueba', (req, res) => {
     console.log(req.body);
     res.status(200).send({
-        message : 'Accion de prueba 2'
+        message: 'Accion de prueba 2'
     })
 });
 
-app.get('/', (req, res) =>{
+app.get('/', (req, res) => {
     res.status(200).send({
-        message : 'API REST'
+        message: 'API REST'
     })
 });
+
+app.use('/api', indv_usuario_route);
 
 app.use('/api', tipoUsuario_route);
 app.use('/api', usuario_route);
-app.use('/api', serv_agencia_route);
-app.use('/api', indv_proveedor_route);
 
-//Exportacion
+
+
+// EXPORTACION
+
 module.exports = app;
